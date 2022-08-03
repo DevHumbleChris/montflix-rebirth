82e520261c90bb1c76702beef5d730fd91697449
DevHumbleChris <devhumblechris254@gmail.com>
2022-08-03 14:55:13 +0300

    managed to setup the tab to work correctly

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,10 @@
 import { useQuery, gql } from "@apollo/client";
 import QueryResult from "./QueryResult";
 import Row from "./Row";
+import { useState } from "react";
 
 export default function Movies() {
+  const [openTab, setOpenTab] = useState(1);
   const TRENDIND_ALL_DAY = gql`
     query Trending($mediaType: String!, $timeWindow: String!) {
       trending(media_type: $mediaType, time_window: $timeWindow) {
@@ -43,6 +45,46 @@ export default function Movies() {
       }
     }
   `;
+  const TRENDIND_ALL_WEEK = gql`
+  query Trending($mediaType: String!, $timeWindow: String!) {
+    trending(media_type: $mediaType, time_window: $timeWindow) {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      known_for {
+        id
+        name
+        title
+        adult
+        backdrop_path
+        poster_path
+        original_language
+        popularity
+        vote_average
+        vote_count
+        first_air_date
+        release_date
+        video
+        media_type
+        gender
+        profile_path
+      }
+    }
+  }
+`;
 
   const { loading, data, error } = useQuery(TRENDIND_ALL_DAY, {
     variables: {
@@ -50,20 +92,50 @@ export default function Movies() {
       timeWindow: "day",
     },
   });
-  
+
+  const { loading: wkLoading, data: wkData, error: wkError } = useQuery(TRENDIND_ALL_WEEK, {
+    variables: {
+      mediaType: "all",
+      timeWindow: "week",
+    },
+  });
+
   return (
     <>
       <div className="my-4 p-3">
         <div className="flex">
           <h3 className="text-2xl">Trending</h3>
           <div className="mx-3 flex text-lg rounded-xl border-2 border-gray-400">
-            <div className="mr-2 hover:bg-[#032541] hover:text-white px-2 hover:rounded-xl">Today</div>
-            <div className="hover:bg-[#032541] hover:text-white px-2 hover:rounded-xl">This Week</div>
+            <div
+              className={
+                openTab === 1
+                  ? "mr-2 bg-[#032541] text-white px-2 rounded-xl"
+                  : "mr-2"
+              }
+              onClick={() => setOpenTab(1)}
+            >
+              Today
+            </div>
+            <div
+              className={
+                openTab === 2 ? "bg-[#032541] text-white px-2 rounded-xl" : ""
+              }
+              onClick={() => setOpenTab(2)}
+            >
+              This Week
+            </div>
           </div>
         </div>
-        <QueryResult data={data} loading={loading} error={error}>
-          <Row data={data} />
-        </QueryResult>
+        {openTab === 1 && (
+          <QueryResult data={data} loading={loading} error={error}>
+            <Row data={data} />
+          </QueryResult>
+        )}
+        {openTab === 2 && (
+          <QueryResult data={wkData} loading={wkLoading} error={wkError}>
+            <Row data={wkData} />
+          </QueryResult>
+        )}
       </div>
     </>
   );
