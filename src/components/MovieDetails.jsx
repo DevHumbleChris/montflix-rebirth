777587d84c403149fb987854849ff7d778ac1a49
DevHumbleChris <devhumblechris254@gmail.com>
2022-08-05 12:35:36 +0300

    Updated the MovieDetails Component

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,21 +1,255 @@
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
 export default function MovieDetails() {
-  return (
-    <>
-      <div
-        className="banner"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url("https://image.tmdb.org/t/p/w500/3TzDP1Jt90tRkVWC3DXGKgNflkh.jpg")`,
-          backgroundPosition: "center center",
-        }}
-      >
-        <div className="banner_contents">
-          <h3 className="banner_title">fdfd</h3>
-          <div className="buttons">
-            <button className="banner_button">Play trailer</button>
+  const { movie_id } = useParams();
+  const MOVIE_DETAILS = gql`
+  query Movie($movieId: Int) {
+  movie(movie_id: $movieId) {
+    details {
+      id
+      adult
+      backdrop_path
+      belongs_to_collection {
+        id
+        name
+        poster_path
+        backdrop_path
+      }
+      budget
+      genres {
+        id
+        name
+      }
+      homepage
+      imdb_id
+      original_language
+      original_title
+      overview
+      popularity
+      poster_path
+      production_companies {
+        id
+        logo_path
+        name
+        origin_country
+      }
+      production_countries {
+        iso_3166_1
+        name
+      }
+      release_date
+      revenue
+      spoken_languages {
+        english_name
+        iso_639_1
+        name
+      }
+      status
+      tagline
+      title
+      video
+      vote_average
+      vote_count
+      videos {
+        iso_639_1
+        iso_3166_1
+        name
+        key
+        site
+        size
+        type
+        published_at
+        official
+        id
+      }
+    }
+    recommendations {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      known_for {
+        id
+        name
+        title
+        adult
+        backdrop_path
+        poster_path
+        original_language
+        popularity
+        vote_average
+        vote_count
+        first_air_date
+        release_date
+        video
+        media_type
+        gender
+        profile_path
+        overview
+      }
+      overview
+    }
+    similar_movies {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+    latest_movies {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+    now_playing {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+    popular_movies {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+    top_rated_movies {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+    upcoming_movies {
+      id
+      name
+      title
+      adult
+      backdrop_path
+      poster_path
+      original_language
+      popularity
+      vote_average
+      vote_count
+      first_air_date
+      release_date
+      video
+      media_type
+      gender
+      profile_path
+      overview
+    }
+  }
+}
+  `;
+  const { loading, error, data } = useQuery(MOVIE_DETAILS, {
+    variables: {
+      movieId: parseInt(movie_id)
+    }
+  });
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  if (error) {
+    return <div>an error occured</div>;
+  }
+  if (data) {
+    const { movie } = data
+    return (
+      <>
+        <div
+          className="banner"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/w500${movie.details?.backdrop_path}")`,
+            backgroundPosition: "center center",
+          }}
+        >
+          <div className="banner_contents">
+            <h3 className="banner_title">{ movie.details?.title }</h3>
+            <div className="buttons">
+              <button className="banner_button">Watch trailer</button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ import {
   XIcon,
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom'
 
 const solutions = [
   {
@@ -85,14 +86,14 @@ export default function Example() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <Link to="/">
               <span className="sr-only">Workflow</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                 alt=""
               />
-            </a>
+            </Link>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -272,13 +273,13 @@ export default function Example() {
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
-                <div>
+                <Link to='/'>
                   <img
                     className="h-8 w-auto"
                     src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                     alt="Workflow"
                   />
-                </div>
+                </Link>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Close menu</span>
